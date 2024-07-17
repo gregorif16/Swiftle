f20b7582fdcf02e31bfee9506f3f5e1ceb372eca
David Ortega <davidortegaiglesias@gmail.com>
2024-07-17 21:17:33 +0200

    add gameView and keyboardView

---
+++ App/Swiftle/Swiftle/KeyboardView.swift
@@ -0,0 +1,35 @@
+//
+//  KeyboardView.swift
+//  Swiftle
+//
+//  Created by David Ortega Iglesias on 17/7/24.
+//
+
+import SwiftUI
+
+struct KeyboardView: View {
+	@ObservedObject var viewModel: ViewModel
+	let columns: [GridItem] = Array(repeating: GridItem(.flexible(minimum: 20), spacing: 0), count: 10)
+	
+    var body: some View {
+		LazyVGrid(columns: columns, spacing: 12) {
+			ForEach(keyboardData, id: \.id) { model in
+				Button {
+					viewModel.addNewLetter(letterModel: model)
+				} label: {
+					Text(model.name)
+						.font(.body)
+				}
+				.frame(width: 34, height: 50)
+				.foregroundColor(model.foregroundColor)
+				.background(model.backgroundColor)
+				.cornerRadius(8)
+			}
+		}
+		.padding(.horizontal, 8)
+    }
+}
+
+#Preview {
+	KeyboardView(viewModel: ViewModel())
+}
